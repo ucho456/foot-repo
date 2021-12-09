c9c6938fc72c9cf9a7550a0f65336e07a09478c6
ucho456 <watapqxyz@gmail.com>
2021-12-09 14:01:20 +0900

    typescript設定&サンプル作成

---
+++ components/Sample.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ fullName }}/{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, ref } from '@nuxtjs/composition-api'
+interface User {
+  firstName: string
+  lastName: string
+}
+export default defineComponent({
+  name: 'Sample',
+  props: {
+    user: {
+      type: Object as () => User,
+      required: true
+    }
+  },
+  setup({ user }) {
+    const fullName = computed(() => `${user.firstName} ${user.lastName}`)
+    const message = ref('This is a message')
+    return { fullName, message }
+  }
+})
+</script>
