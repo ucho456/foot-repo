dab9609ade36dc535854d5f1ee2041346ee05c20
ucho456 <watapqxyz@gmail.com>
2021-12-10 09:25:42 +0000

    メールアドレス・パスワードのテキストフィールド作成

---
+++ src/components/molecules/TextFieldEmail.vue
@@ -0,0 +1,34 @@
+<template>
+  <ValidationProvider v-slot="{ errors }" rules="required|email">
+    <BaseTextField
+      :value="value"
+      :icon="'mdi-email'"
+      :label="'メール'"
+      :type="'email'"
+      :error-message="errors[0]"
+      @input="handleInput"
+    />
+  </ValidationProvider>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import BaseTextField from '@/components/atoms/BaseTextField.vue'
+
+export default defineComponent({
+  name: 'TextFieldEmail',
+
+  components: {
+    BaseTextField
+  },
+
+  props: {
+    value: { type: String, default: '' }
+  },
+
+  setup(_, ctx) {
+    const handleInput = (value: string): void => ctx.emit('input', value)
+    return { handleInput }
+  }
+})
+</script>
---
+++ src/components/molecules/TextFieldPassword.vue
@@ -0,0 +1,34 @@
+<template>
+  <ValidationProvider v-slot="{ errors }" rules="required|minPassword:4">
+    <BaseTextField
+      :value="value"
+      :icon="'mdi-lock'"
+      :label="'パスワード'"
+      :type="'password'"
+      :error-message="errors[0]"
+      @input="handleInput"
+    />
+  </ValidationProvider>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import BaseTextField from '@/components/atoms/BaseTextField.vue'
+
+export default defineComponent({
+  name: 'TextFieldPassword',
+
+  components: {
+    BaseTextField
+  },
+
+  props: {
+    value: { type: String, default: '' }
+  },
+
+  setup(_, ctx) {
+    const handleInput = (value: string): void => ctx.emit('input', value)
+    return { handleInput }
+  }
+})
+</script>
