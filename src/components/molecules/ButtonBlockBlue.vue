7707e2dd32f245882fd422a98d6785582e1f53e4
ucho456 <watapqxyz@gmail.com>
2021-12-10 07:51:42 +0000

    ボタンコンポーネント作成

---
+++ src/components/molecules/ButtonBlockBlue.vue
@@ -0,0 +1,38 @@
+<template>
+  <BaseButton
+    :block-flg="true"
+    :color="'darkBlue'"
+    :disabled="disabled"
+    :icon="icon"
+    :left-flg="true"
+    :loading="loading"
+    :text="text"
+    :white-text-flg="true"
+    @click="handleClick"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import BaseButton from '@/components/atoms/BaseButton.vue'
+
+export default defineComponent({
+  name: 'ButtonBlockBlue',
+
+  components: {
+    BaseButton
+  },
+
+  props: {
+    disabled: { type: Boolean, default: false },
+    icon: { type: String, default: '' },
+    loading: { type: Boolean, default: false },
+    text: { type: String, default: '' }
+  },
+
+  setup(_, ctx) {
+    const handleClick = (): void => ctx.emit('click')
+    return { handleClick }
+  }
+})
+</script>
---
+++ src/components/molecules/ButtonBlockWhite.vue
@@ -0,0 +1,34 @@
+<template>
+  <BaseButton
+    :block-flg="true"
+    :color="'darkBlue'"
+    :icon="icon"
+    :left-flg="true"
+    :outlined-flg="true"
+    :text="text"
+    @click="handleClick"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import BaseButton from '@/components/atoms/BaseButton.vue'
+
+export default defineComponent({
+  name: 'ButtonBlockWhite',
+
+  components: {
+    BaseButton
+  },
+
+  props: {
+    icon: { type: String, default: '' },
+    text: { type: String, default: '' }
+  },
+
+  setup(_, ctx) {
+    const handleClick = (): void => ctx.emit('click')
+    return { handleClick }
+  }
+})
+</script>
