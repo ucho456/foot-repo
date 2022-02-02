8adf23a794b4c43becb6689649c8bb19eced4846
ucho456 <watapqxyz@gmail.com>
2022-02-02 08:19:49 +0000

    ReportsHeader修正

--- src/components/organisms/ReportsHeader.vue
+++ src/components/organisms/ReportsHeader.vue
@@ -4,7 +4,7 @@
       <v-col class="text-center"> {{ utcDate.substring(0, 10) }} {{ competitionName }}</v-col>
     </v-row>
     <v-row class="mt-n4">
-      <v-col cols="3">
+      <v-col cols="5">
         <v-row justify="center">
           <v-img
             class="rounded-circle"
@@ -14,15 +14,8 @@
           />
         </v-row>
       </v-col>
-      <v-col class="text-right text-truncate" cols="3">
-        <div>{{ homeTeamName }}</div>
-        <div>{{ homeTeamScore }}</div>
-      </v-col>
-      <v-col class="text-left text-truncate" cols="3">
-        <div>{{ awayTeamName }}</div>
-        <div>{{ awayTeamScore }}</div>
-      </v-col>
-      <v-col cols="3">
+      <v-col cols="2"></v-col>
+      <v-col cols="5">
         <v-row justify="center">
           <v-img
             class="rounded-circle"
@@ -31,6 +24,21 @@
             :src="`https://crests.football-data.org/${awayTeamId}.svg`"
           />
         </v-row>
+      </v-col>
+      <v-col class="text-center text-truncate" cols="5">
+        <div>{{ homeTeamName }}</div>
+        <div>{{ homeTeamScore }}</div>
+        <div v-if="isPK">{{ homeTeamPenalty }}</div>
+      </v-col>
+      <v-col class="text-center text-truncate" cols="2">
+        <div>vs</div>
+        <div>-</div>
+        <div v-if="isPK">PK</div>
+      </v-col>
+      <v-col class="text-center text-truncate" cols="5">
+        <div>{{ awayTeamName }}</div>
+        <div>{{ awayTeamScore }}</div>
+        <div v-if="isPK">{{ awayTeamPenalty }}</div>
       </v-col>
     </v-row>
   </v-container>
@@ -45,12 +53,22 @@ export default defineComponent({
   props: {
     awayTeamId: { type: Number, default: 0 },
     awayTeamName: { type: String, default: '' },
+    awayTeamPenalty: { type: Number, default: 0 },
     awayTeamScore: { type: Number, default: 0 },
     competitionName: { type: String, default: '' },
     homeTeamId: { type: Number, default: 0 },
     homeTeamName: { type: String, default: '' },
+    homeTeamPenalty: { type: Number, default: 0 },
     homeTeamScore: { type: Number, default: 0 },
     utcDate: { type: String, default: '' }
+  },
+
+  setup(props) {
+    const isPK =
+      props.homeTeamScore === props.awayTeamScore &&
+      props.homeTeamPenalty !== 0 &&
+      props.awayTeamPenalty !== 0
+    return { isPK }
   }
 })
 </script>
