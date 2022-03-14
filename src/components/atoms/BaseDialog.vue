6e8c2b885b85a2305de71d81b3f7ad159bebaa93
ucho456 <watapqxyz@gmail.com>
2022-03-14 06:49:31 +0000

    ダイアログを作成

---
+++ src/components/atoms/BaseDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      :max-width="maxWidth"
+      :persistent="persistent"
+      :value="show"
+      width="90%"
+      @input="handleClose"
+    >
+      <v-card>
+        <slot></slot>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="handleClose"> 閉じる </v-btn>
+          <v-btn v-if="decideButtonText !== ''" color="primary" text @click="handleDecide">
+            {{ decideButtonText }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'BaseDialog',
+
+  props: {
+    decideButtonText: { type: String, default: '' },
+    maxWidth: { type: Number, default: 0 },
+    persistent: { type: Boolean, default: false },
+    show: { type: Boolean, default: false }
+  },
+
+  setup(_, ctx) {
+    const handleDecide = (): void => ctx.emit('decide')
+    const handleClose = (): void => ctx.emit('close')
+    return { handleDecide, handleClose }
+  }
+})
+</script>
