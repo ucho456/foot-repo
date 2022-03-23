2b91d1a837f238d7f72b7bb46cc3dd0a1ccc5538
ucho456 <watapqxyz@gmail.com>
2022-03-23 05:22:33 +0000

    firebaseApiKeyなどを直記述

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyCiH-SePvxwZVAKGkK0sbqbD5ZOXtAH5xw',
+  authDomain: 'foot-repo.firebaseapp.com',
+  projectId: 'foot-repo',
+  storageBucket: 'foot-repo.appspot.com',
+  messagingSenderId: '289946075434',
+  appId: '1:289946075434:web:ce3f88ded80f04a613de53',
+  measurementId: 'G-FFB7N3Q6LY'
 }
 
 const firebase = !getApps().length ? initializeApp(config) : getApp()
