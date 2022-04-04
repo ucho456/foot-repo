dc2378e6c85201aa6d85b27521ae634060919763
ucho456 <watapqxyz@gmail.com>
2022-04-04 03:44:08 +0000

    default.vue修正

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,29 +2,32 @@
   <v-app class="light-grey">
     <v-app-bar class="white--text" app color="primary" fixed>
       <v-container class="align-center d-flex">
-        <v-toolbar-title v-text="'Foot-Repo'" />
+        <v-toolbar-title class="hover" @click="pushToHome" v-text="'Foot-Repo'" />
         <v-spacer />
-        <v-btn class="white--text" icon @click.stop="toggleDrawer">
-          <v-icon>mdi-menu</v-icon>
-        </v-btn>
+        <v-btn class="white--text" color="accent" elevation="0" to="/reports/search"> 投稿 </v-btn>
+        <ClientOnly>
+          <v-list-item-avatar class="hover" @click.stop="toggleDrawer">
+            <v-img v-if="currentUser" :src="currentUser.imageUrl"></v-img>
+            <v-img v-else :src="noAvatarImage"></v-img>
+          </v-list-item-avatar>
+        </ClientOnly>
       </v-container>
     </v-app-bar>
     <v-main>
       <v-container>
         <v-row>
-          <v-col md="9" sm="8"><nuxt /></v-col>
-          <v-col md="3" sm="4"><SideContainer /></v-col>
+          <v-col cols="12" sm="8" md="9"><nuxt /></v-col>
+          <v-col cols="12" sm="4" md="3"><SideContainer /></v-col>
         </v-row>
       </v-container>
     </v-main>
+    <v-footer class="white--text" absolute app color="fotter">
+      <v-container class="align-center d-flex">
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-container>
+    </v-footer>
     <v-navigation-drawer v-model="showFlg" fixed right temporary>
       <v-list>
-        <v-list-item class="px-2" :to="'/'">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-title>John Leider</v-list-item-title>
-        </v-list-item>
         <v-list-item
           v-for="item in navigationDrawerItems"
           :key="item.id"
@@ -39,26 +42,27 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
+        <ClientOnly>
+          <v-list-item v-if="currentUser" class="px-2 ml-2" @click="logout">
+            <v-list-item-action>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="'ログアウト'" />
+            </v-list-item-content>
+          </v-list-item>
+        </ClientOnly>
       </v-list>
-      <template #append>
-        <div class="px-5">
-          <div>底</div>
-        </div>
-      </template>
     </v-navigation-drawer>
-    <v-footer class="white--text" :absolute="true" app color="fotter">
-      <v-container class="align-center d-flex">
-        <span>&copy; {{ new Date().getFullYear() }}</span>
-      </v-container>
-    </v-footer>
     <Snackbar v-bind="snackbar" />
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent, computed, ref, useRouter } from '@nuxtjs/composition-api'
+import { getAuth, signOut } from 'firebase/auth'
 import SideContainer from '@/components/organisms/SideContainer.vue'
-import { navigationDrawerItems } from '@/utils/navigationDrawerItems'
+import useCurrentUser from '@/utils/useCurrentUser'
 import Snackbar from '@/components/molecules/Snackbar.vue'
 import useSnackbar from '@/utils/useSnackbar'
 
@@ -71,10 +75,66 @@ export default defineComponent({
   },
 
   setup() {
-    const { snackbar } = useSnackbar()
+    const currentUser = useCurrentUser()
+    const noAvatarImage = require('@/assets/no_avatar.png')
+    const router = useRouter()
+    const pushToHome = () => router.push('/')
+    const { snackbar, openSnackbar } = useSnackbar()
+
+    const navigationDrawerItems = computed(() => {
+      const home = { icon: 'mdi-home', title: 'ホーム', to: '/' }
+      const myPage = { icon: 'mdi-account', title: 'マイページ', to: '/my-page' }
+      const reportNew = { icon: 'mdi-pencil-plus', title: '選手採点投稿', to: '/reports/search' }
+      const database = { icon: 'mdi-chart-bar', title: 'データベース', to: '/databases/' }
+      const chatRoom = { icon: 'mdi-chat-processing', title: 'チャットルーム', to: '/rooms/' }
+      const contact = { icon: 'mdi-face-agent', title: '問い合わせ', to: '/contact' }
+      const login = { icon: 'mdi-login', title: 'ログイン', to: '/login' }
+      const about = { icon: 'mdi-information', title: '当サイトについて', to: '/about' }
+      const signup = { icon: 'mdi-account-plus', title: '新規登録', to: '/signup' }
+      return currentUser.value
+        ? [
+            { id: 1, ...home },
+            { id: 2, ...myPage },
+            { id: 3, ...reportNew },
+            { id: 4, ...database },
+            { id: 5, ...chatRoom },
+            { id: 6, ...about },
+            { id: 7, ...contact }
+          ]
+        : [
+            { id: 1, ...home },
+            { id: 2, ...login },
+            { id: 3, ...signup },
+            { id: 4, ...reportNew },
+            { id: 5, ...database },
+            { id: 6, ...chatRoom },
+            { id: 7, ...about },
+            { id: 8, ...contact }
+          ]
+    })
     const showFlg = ref(false)
     const toggleDrawer = (): boolean => (showFlg.value = !showFlg.value)
-    return { snackbar, navigationDrawerItems, showFlg, toggleDrawer }
+
+    const auth = getAuth()
+    const logout = (): void => {
+      signOut(auth)
+        .then(() => {
+          showFlg.value = false
+          router.push('/')
+          openSnackbar('success', 'ログアウトしました。')
+        })
+        .catch(() => openSnackbar('failure', 'ログアウトに失敗しました。'))
+    }
+    return {
+      currentUser,
+      noAvatarImage,
+      snackbar,
+      navigationDrawerItems,
+      showFlg,
+      toggleDrawer,
+      pushToHome,
+      logout
+    }
   }
 })
 </script>
@@ -82,5 +142,11 @@ export default defineComponent({
 <style lang="scss" scoped>
 .light-grey {
   background: #eceff1;
+}
+.hover {
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
 }
 </style>
