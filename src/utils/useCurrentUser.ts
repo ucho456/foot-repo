1489804e93cb44e84371dcf9ba66940556eee179
ucho456 <watapqxyz@gmail.com>
2022-04-04 07:50:43 +0000

    不要なコメントを削除

--- src/utils/useCurrentUser.ts
+++ src/utils/useCurrentUser.ts
@@ -9,30 +9,3 @@ const useCurrentUser = (): Ref<CurrentUser | null> => {
 }
 
 export default useCurrentUser
-
-// export const login = async (
-//   providerType: ProviderType,
-//   email: string,
-//   password: string
-// ): Promise<InitCurrentUser | null> => {
-//   const auth = getAuth()
-//   const provider = providerType === 'twitter' ? new TwitterAuthProvider() : new GoogleAuthProvider()
-//   const userCredential =
-//     providerType === 'email'
-//       ? await signInWithEmailAndPassword(auth, email, password)
-//       : await signInWithPopup(auth, provider)
-
-//   const uid = userCredential.user.uid
-//   const publicProfileRef = await doc(db, 'users', uid)
-//   const publicProfileSnap = await getDoc(publicProfileRef)
-//   if (publicProfileSnap.exists()) {
-//     return null
-//   } else {
-//     const initCurrentUser = {
-//       uid: userCredential.user.uid,
-//       name: userCredential.user.displayName,
-//       photoUrl: userCredential.user.photoURL
-//     }
-//     return initCurrentUser
-//   }
-// }
