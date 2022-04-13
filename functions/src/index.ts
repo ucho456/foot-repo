c2924438fe861b001107229348c4d3505fdc51ce
ucho456 <watapqxyz@gmail.com>
2022-04-13 02:46:13 +0000

    functionsで環境変数を使用

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 // firebase functions:shell シェル起動
+// firebase functions:config:set foot-repo.key="value" 環境変数設定
+// firebase functions:config:get > .runtimeconfig.json 環境変数書き出し
 
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
+const env = functions.config()['foot-repo']
 
 export const initAuth = functions
   .region('asia-northeast1')
@@ -31,6 +34,7 @@ export const aaa = functions
   .region('asia-northeast1')
   .pubsub.schedule('every 1 minutes')
   .onRun((context) => {
-    console.log('this will be run every 1 minutes!', context)
+    console.log(context)
+    console.log('token', env.football_token)
     return null
   })
