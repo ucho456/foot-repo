5fb4910cca75c550faadb047d0aff7af7d2887de
ucho456 <watapqxyz@gmail.com>
2022-06-22 06:35:15 +0000

    timeline 表示

--- src/components/organisms/SideContainer.vue
+++ src/components/organisms/SideContainer.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-card class="light-grey" elevation="0">
-    <!-- <v-container>
-      <v-row>
-        <v-col>
-          <a
-            class="twitter-timeline"
-            data-lang="en"
-            data-height="500"
-            href="https://twitter.com/FootRepo?ref_src=twsrc%5Etfw"
-            >by FootRepo</a
-          >
-        </v-col>
-      </v-row>
-    </v-container>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
-  </v-card>
+  <v-container>
+    <v-card outlined>
+      <v-container>
+        <a
+          class="twitter-timeline"
+          data-height="400"
+          data-lang="ja"
+          data-dnt="true"
+          href="https://twitter.com/FootRepo?ref_src=twsrc%5Etfw"
+        ></a>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  name: 'SideContainer'
+  name: 'SideContainer',
+
+  setup() {
+    const createTweetScript = () => {
+      const tweetScript = document.createElement('script')
+      tweetScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+      tweetScript.setAttribute('async', 'true')
+      document.head.appendChild(tweetScript)
+    }
+    if (process.client) {
+      createTweetScript()
+    }
+  }
 })
 </script>
-
-<style lang="scss" scoped>
-.light-grey {
-  background: #eceff1;
-}
-</style>
