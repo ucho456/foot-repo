51047c18c04a23070110bbe4a7d7824ee67fab2a
ucho456 <watapqxyz@gmail.com>
2022-06-27 04:59:50 +0000

    console.log 許可

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   rules: {
+    'no-console': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error'
   }
