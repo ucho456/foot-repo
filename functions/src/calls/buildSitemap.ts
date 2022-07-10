5672e0bf312d82dfdfe08ec15ca7bbc97fe711e4
ucho456 <watapqxyz@gmail.com>
2022-07-10 15:32:16 +0000

    sitemap.xml リクエスト成功

--- functions/src/calls/buildSitemap.ts
+++ functions/src/calls/buildSitemap.ts
@@ -2,22 +2,17 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
-const buildSitemap = functions.region('asia-northeast1').https.onRequest(async (_, res) => {
+const buildSitemap = functions.https.onRequest(async (_, res) => {
   const sitemapHeader = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
-
   const baseURL = `<url><loc>https://foot-repo.com/</loc></url>`
-
-  const rSnapshot = await admin.database().ref('reports').once('value')
-  const reports = rSnapshot.val()
-  const reportURLs = Object.keys(reports).reduce((acc, url) => {
-    acc = acc + `<url><loc>https://foot-repo.com/${url}</loc></url>`
-    return acc
-  }, '')
-
+  const rRef = admin.firestore().collection('reports')
+  const rSnapshot = await rRef.get()
+  let reportURLs = ''
+  rSnapshot.forEach((doc) => {
+    if (doc.exists) reportURLs += `<url><loc>https://foot-repo.com/reports/${doc.id}</loc></url>`
+  })
   const sitemapFooter = `</urlset>`
-
   const sitemapString = sitemapHeader + baseURL + reportURLs + sitemapFooter
-
   res.set('Content-Type', 'text/xml')
   res.status(200).send(sitemapString)
 })
