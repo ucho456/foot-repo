d6822fca55bac25dcd96551ebd0834473611972b
ucho456 <watapqxyz@gmail.com>
2022-07-13 08:30:43 +0000

    process.client 削除

--- src/plugins/firebase-auth.js
+++ src/plugins/firebase-auth.js
@@ -8,27 +8,44 @@ export default defineNuxtPlugin((_, inject) => {
 
   inject('loginUser', loginUser)
 
-  let unsubscribe
-  if (process.client) {
-    const auth = getAuth()
-    unsubscribe = onAuthStateChanged(auth, async (authUser) => {
-      if (authUser) {
-        const user = await fetchUser(authUser.uid)
-        loginUser.value = user
-          ? {
-              uid: user.id,
-              name: user.name,
-              imageUrl: user.imageUrl,
-              competitionId: user.competitionId,
-              team: user.team,
-              greet: user.greet
-            }
-          : null
-      } else {
-        loginUser.value = null
-      }
-    })
-  }
+  const auth = getAuth()
+  const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+    if (authUser) {
+      const user = await fetchUser(authUser.uid)
+      loginUser.value = user
+        ? {
+            uid: user.id,
+            name: user.name,
+            imageUrl: user.imageUrl,
+            competitionId: user.competitionId,
+            team: user.team,
+            greet: user.greet
+          }
+        : null
+    } else {
+      loginUser.value = null
+    }
+  })
+  // if (process.client) {
+  //   const auth = getAuth()
+  //   unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+  //     if (authUser) {
+  //       const user = await fetchUser(authUser.uid)
+  //       loginUser.value = user
+  //         ? {
+  //             uid: user.id,
+  //             name: user.name,
+  //             imageUrl: user.imageUrl,
+  //             competitionId: user.competitionId,
+  //             team: user.team,
+  //             greet: user.greet
+  //           }
+  //         : null
+  //     } else {
+  //       loginUser.value = null
+  //     }
+  //   })
+  // }
 
   onGlobalSetup(() => {
     provide(LoginUserKey, loginUser)
