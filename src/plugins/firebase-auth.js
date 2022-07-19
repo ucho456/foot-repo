2458c26951e9f230045704fd925ab767244dda0c
ucho456 <watapqxyz@gmail.com>
2022-07-19 05:40:01 +0000

    onAuthStateChangedをpromiseで

--- src/plugins/firebase-auth.js
+++ src/plugins/firebase-auth.js
@@ -3,31 +3,31 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { LoginUserKey } from '@/utils/useLoginUser'
 import { fetchUserPriorityFromCashe } from '@/db/users'
 
-export default defineNuxtPlugin((_, inject) => {
+export default defineNuxtPlugin(async (_, inject) => {
   const loginUser = ref(null)
 
   inject('loginUser', loginUser)
 
-  const auth = getAuth()
-  const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
-    if (authUser) {
-      const p0 = performance.now()
-      const user = await fetchUserPriorityFromCashe(authUser.uid)
-      loginUser.value = user
-        ? {
-            uid: user.id,
-            name: user.name,
-            imageUrl: user.imageUrl,
-            competitionId: user.competitionId,
-            team: user.team,
-            greet: user.greet
-          }
-        : null
-      const p1 = performance.now()
-      console.log('performance', p1 - p0)
-    } else {
-      loginUser.value = null
-    }
+  const unsubscribe = await new Promise((resolve) => {
+    const auth = getAuth()
+    const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+      if (authUser) {
+        const user = await fetchUserPriorityFromCashe(authUser.uid)
+        loginUser.value = user
+          ? {
+              uid: user.id,
+              name: user.name,
+              imageUrl: user.imageUrl,
+              competitionId: user.competitionId,
+              team: user.team,
+              greet: user.greet
+            }
+          : null
+      } else {
+        loginUser.value = null
+      }
+      resolve(unsubscribe)
+    })
   })
 
   onGlobalSetup(() => {
