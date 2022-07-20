38a0269861668add606ab1fca008f8631b93643a
ucho456 <watapqxyz@gmail.com>
2022-07-20 06:40:46 +0000

    error.vue 復活

---
+++ src/layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app class="o-grey">
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <Snackbar v-bind="snackbar" />
+  </v-app>
+</template>
+<script lang="ts">
+/** check */
+import { defineComponent } from '@nuxtjs/composition-api'
+import useSnackbar from '@/utils/useSnackbar'
+import Snackbar from '@/components/molecules/Snackbar.vue'
+export default defineComponent({
+  name: 'Grey',
+  components: {
+    Snackbar
+  },
+  setup() {
+    const { snackbar } = useSnackbar()
+
+    return { snackbar }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.o-grey {
+  background: #eceff1;
+}
+</style>
